5bb6f9203a9d0de5ecc1923847bc35e0da8b2d0a
2Fat4Dat <34524562+2Fat4Dat@users.noreply.github.com>
2017-12-14 01:29:14 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -43,7 +43,7 @@ var l_background = "backgrounds/videos/glimmering.webm";
 Place one or more images
 Multiple images will automatically shuffle between them*/
 var l_bgImages = [
-	"backgrounds/images/touhou01.jpg",
+	"backgrounds/images/coolload.jpg",
 ];
 
 /*Random background images order?*/
