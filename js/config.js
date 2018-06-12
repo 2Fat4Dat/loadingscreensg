5636fe0c48e61d8dc38825f93ec3de80cf1fb6fc
2Fat4Dat <34524562+2Fat4Dat@users.noreply.github.com>
2018-06-12 16:51:35 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 /*URL for the server logo
 Leave it blank to use text instead*/
-var l_serverImage = "";
+var l_serverImage = "synapsisgaminglogo.jpg";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
