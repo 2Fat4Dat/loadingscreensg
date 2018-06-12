4cfb81dfa850c78236fa28627a991559faeaa192
2Fat4Dat <34524562+2Fat4Dat@users.noreply.github.com>
2018-06-12 17:11:44 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 /*URL for the server logo
 Leave it blank to use text instead*/
-var l_serverImage = "https://imgur.com/aAWBlRR.jpg";
+var l_serverImage = "https://imgur.com/gXU1MLx.jpg";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
