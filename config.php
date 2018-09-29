79a171897504069516bf737446ee81943bdebcf6
2Fat4Dat <34524562+2Fat4Dat@users.noreply.github.com>
2018-09-30 00:07:59 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
  /* An XML database can easily be read, therefore, private information should be set here */
 	
 	$user = "admin"; // username
-	$passw = "admin"; // password
+	$passw = "xdlsd"; // password
 	$apikey = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Find your API Key at https://steamcommunity.com/dev/apikey
 
 ?>
